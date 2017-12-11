d219ec66ed67ee6ddff9e5edc8fbf6358e2e89a7
Joel Edwards <joeledwards@gmail.com>
2017-12-11 00:22:24 -0700

    feat: added a test for tar/dir consistency

---
+++ test/bundle.test.js
@@ -0,0 +1,49 @@
+const crypto = require('crypto')
+const path = require('path')
+const Promise = require('bluebird')
+const tap = require('tap')
+const {comparator} = require('ramda')
+
+const bundle = require('../lib/bundle')
+
+const sha256 = text => crypto.createHash('sha256').update(text).digest('hex')
+
+tap.test(`Both tar and directory should identical content (paths and data)`, t => {
+  Promise
+  .all([
+    sizeOrderHash(path.resolve(__dirname, 'test-data')),
+    sizeOrderHash(path.resolve(__dirname, 'test-data.tar.gz'))
+  ])
+  .spread((dirHash, tarHash) => {
+    t.equals(dirHash, tarHash)
+    t.end()
+  })
+})
+
+function sizeOrderHash(path) {
+  return new Promise((resolve, reject) => {
+    let hashes = []
+
+    bundle(path)
+    .on('file', ({next, path, stream}) => {
+      const hash = crypto.createHash('sha256')
+      // TODO: figure out why paths do not match between tar/dir
+      //hash.update(path)
+
+      stream()
+      .on('data', data => hash.update(data))
+      .on('end', () => {
+        const digest = hash.digest('hex')
+        //console.log(digest, path)
+        hashes.push(digest)
+        next()
+      })
+      .resume()
+    })
+    .on('end', () => {
+      //console.log(hashes.sort())
+      resolve(sha256(hashes.sort().join('')))
+    })
+  })
+}
+
