44b562775bc1e6910ee9f2436c9beb6454cfa619
Joel Edwards <joeledwards@gmail.com>
2018-04-19 09:59:02 -0600

    fix: linting errors

--- commands/package-count.js
+++ commands/package-count.js
@@ -10,29 +10,29 @@ function handler ({registryUrl}) {
   const {blue, grey, orange, red, emoji} = require('@buzuli/color')
 
   axios
-  .get(registryUrl, {
-    validateStatus: _s => true
-  })
-  .then(({status, data = {}}) => {
-    if (status === 200) {
-      const {doc_count: docCount, doc_del_count: delCount} = data
-      if (r.isNil(docCount) || r.isNil(delCount)) {
-        console.error(red(`URL does not appear to be a valid registry (bad response format) : ${grey(JSON.stringify(data))}`))
-        process.exit(1)
+    .get(registryUrl, {
+      validateStatus: _s => true
+    })
+    .then(({status, data = {}}) => {
+      if (status === 200) {
+        const {doc_count: docCount, doc_del_count: delCount} = data
+        if (r.isNil(docCount) || r.isNil(delCount)) {
+          console.error(red(`URL does not appear to be a valid registry (bad response format) : ${grey(JSON.stringify(data))}`))
+          process.exit(1)
+        } else {
+          console.info(`Package count: ~ ${orange(Math.max(docCount - delCount, 0))}`)
+        }
       } else {
-        console.info(`Package count: ~ ${orange(Math.max(docCount - delCount, 0))}`)
+        console.error(`[${red(status)}] ${grey(JSON.stringify(data))}`)
+        process.exit(1)
       }
-    } else {
-      console.error(`[${red(status)}] ${grey(JSON.stringify(data))}`)
-      process.exit(1)
-    }
-  })
-  .catch(error => {
-    console.error(error)
-    console.error(
-      red(`Error fetching package count for ${blue(registryUrl)}`) +
+    })
+    .catch(error => {
+      console.error(error)
+      console.error(
+        red(`Error fetching package count for ${blue(registryUrl)}`) +
       emoji.inject(` ... details above :point_up:`)
-    )
-    process.exit(1)
-  })
+      )
+      process.exit(1)
+    })
 }
